8e0c0b9dc1dd1812ad3937a0f35346cdb4819ce7
Franco Cherubini <cherubini.franco@hotmail.com>
2022-05-05 22:49:49 -0300

    Initial code

---
+++ script.js
@@ -0,0 +1,44 @@
+const score = document.getElementById("score");
+let playerScore = 0;
+let botScore = 0;
+let botChoise;
+
+const updateScore = () =>{
+    score.innerHTML = playerScore + ":" + botScore;
+    botChoise = random(0,3);
+    switch (botChoise){
+        case 0: botChoise = "Rock";
+        break;
+        case 1: botChoise = "Scissors";
+        break;
+        case 2: botChoise = "Paper"
+    }
+    console.log("The bot if going to chose: " + botChoise);
+}
+
+const random = (A, B) =>{
+    let X = Math.floor(Math.random() * B + A);
+    return X;
+}
+
+const msje = (A,B,C) =>{
+    console.log("SICAHR GATO " + A + " beats " + B + " you " + C);
+    if (C == "lose"){
+        botScore++;
+    } else {
+        playerScore++;
+    }
+}
+
+const game = x => {
+    if(x == botChoise){
+        console.log("Empate");
+    }else if (x == 'Rock' && botChoise == 'Scissors' || x == 'Paper' && botChoise == 'Rock' || x== 'Scissors' && botChoise == 'Paper'){
+        msje(x,botChoise, 'win');
+    }else{
+        msje(botChoise, x, 'lose')
+    }
+    updateScore();
+}
+
+updateScore();
